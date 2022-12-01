644f47bee9ddc9053ac69bf6852ed9411f600bbd
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-12-01 16:46:50 -0600

    user

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -2,11 +2,11 @@ variable "test" {
   default = "hello"
 }
 output "test" {
-  value = "var.test"
+  value = var.test
 }
 variable "test1" {
 
 }
 output "test1" {
-  value = "var.test1"
+  value = var.test1
 }
