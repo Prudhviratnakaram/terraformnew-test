8763484b69ffdc344ccf3ffcf36092abfb906d86
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-12-01 19:15:06 -0600

    user

---
+++ example1/security-group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "web"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "web"
+  }
+}
+
